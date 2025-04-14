6f86ddcbc935a8b8b2f2db5b0d32b6e2e99d4d88
whowouldwin <leranetwork@gmail.com>
2025-04-14 18:04:09 +0300

    feat: implement partialUsingArguments

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -215,8 +215,10 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return function partiallyApplied(...args2) {
+    return fn(...args1, ...args2);
+  };
 }
 
 /**
